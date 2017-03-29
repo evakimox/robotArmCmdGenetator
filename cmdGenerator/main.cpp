0be559ba8968e82903be9117d9448169550c9f99
evakimox <zhangjn@bu.edu>
2017-03-29 17:45:18 -0400

    Serial sender skeleton (no serial indeed)
    PC does not have that serial library, will need to try on Pi3

--- cmdGenerator/main.cpp
+++ cmdGenerator/main.cpp
@@ -1,13 +1,20 @@
 #include "arc.cpp"
 #include "adjustB.cpp"
 #include <unistd.h>
+#include <fstream>
+#include <stdlib.h>
 void moveTopoint(double x, double y, double z, bool fromLeft);
-int main(){
-  double x;
-  double y;
-  double z;
-  bool Left;
-  cin>>x>>y>>z>>Left;
+int main(int argc, char *argv[]){
+  if(argc <5){
+    cout<<"Not Enough argument, try agian"<<endl;
+    return 0;
+  }
+  
+  //I have enough argument branch
+  double x = atoi(argv[1]);
+  double y = atoi(argv[2]);
+  double z = atoi(argv[3]);
+  bool Left = atoi(argv[4]);
 
   moveTopoint(x,y,z,Left);
   return 0;
@@ -24,6 +31,7 @@ void moveTopoint(double x, double y, double z, bool fromLeft){
   int C = iniC;
   adjustABC(x,y,z,A,B,C);
 
+  //final position recorded
   int diffB = abs(B-iniB);
   int iniBsmol = (iniB-B)>0?1:-1;
   int diffC = abs(C-iniC);
@@ -37,15 +45,23 @@ void moveTopoint(double x, double y, double z, bool fromLeft){
   int ServoB = B+10;
   int ServoC = C;
 
+  //prepare file to be send to arduino
+  ofstream Commands;
+  Commands.open("Commands.txt");
+  
+  //check if position reachable
   if(y>0&& ServoA>22&&ServoA<144&& ServoC>25&&ServoC<170&& ServoB>0&&ServoB<191-C){
   }
   else{
-    cout<<"Bad position"<<endl;
+    /*branch not reachable position, tell arduino not to operate*/
+    //write to file
+    Commands<<"Bad position"<<endl;
     return;
   }
-  
-    
-  cout<<"Destination: "<<A+83<<" a "<<B+10<<" b "<<C<<" c\n"<<endl;
+
+  /*branch reachable*/
+  //note to self
+  cout<<"Destination: "<<A+83<<" a "<<B+10<<" b "<<C<<" c"<<endl;
 
   //select mode for A operation
   if(fromLeft){
@@ -53,7 +69,8 @@ void moveTopoint(double x, double y, double z, bool fromLeft){
     diffA = abs(A-iniA);
     iniAsmol = (iniA-A)>0?1:-1;
     for(int i = 0;i<60;i++){
-      cout<<83-i<<" a "<<endl;
+      //write to file
+      Commands<<83-i<<" a "<<endl;
     }
   }
   else{
@@ -61,23 +78,23 @@ void moveTopoint(double x, double y, double z, bool fromLeft){
     diffA = abs(A-iniA);
     iniAsmol = (iniA-A)>0?1:-1;
     for(int i =0;i<60;i++){
-      cout<<83+i<<" a "<<endl;
+      //write to file
+      Commands<<83+i<<" a "<<endl;
     }
   }
 
   
   for(int i=0;i<=diffC;i++){
-    //cout<<A+83<<" a "<<iniB+10<<" b "<<iniC-(i*iniCsmol)<<" c "<<endl;
-    cout<<iniC-(i*iniCsmol)<<" c "<<endl;
-    usleep(30000);
+    //write to file
+    Commands<<iniC-(i*iniCsmol)<<" c "<<endl;
   }
   for(int i=0;i<=diffB;i++){
-    //cout<<A+83<<" a "<<iniB-(i*iniBsmol)+10<<" b "<<C<<" c "<<endl;
-    cout<<iniB-(i*iniBsmol)+10<<" b "<<endl;
-    usleep(30000);
+    //write to file
+    Commands<<iniB-(i*iniBsmol)+10<<" b "<<endl;
   }
   for(int i=0;i<=diffA;i++){
-    cout<<iniA-(i*iniAsmol)+83<<" a "<<endl;
-    usleep(30000);
+    //write to file
+    Commands<<iniA-(i*iniAsmol)+83<<" a "<<endl;
   }
+  Commands.close();
 }
